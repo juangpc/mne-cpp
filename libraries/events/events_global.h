7944ae7675b0b8ee35cbe4dca507bc231b61266f
juangpc <juangpc@gmail.com>
2021-08-05 16:41:53 -0400

    fix small date typo in events_global.h

--- libraries/events/events_global.h
+++ libraries/events/events_global.h
@@ -7,7 +7,7 @@
  *
  * @section  LICENSE
  *
- * Copyright (C) 2012, Juan GPC. All rights reserved.
+ * Copyright (C) 2021, Juan GPC. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that
  * the following conditions are met:
